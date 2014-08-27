00155d8cbb1435ca7c8b6d7741f4fb41637c654a
Nobita <unvisityboss@gmail.com>
2014-08-27 05:01:13 +0000

    pure js and css

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ### 使用說明
 ===
 - 先下 npm install 來安裝各種需要的套件
-- 再下 gulp watch ，就可以開始實作 coffee 和 scss 了。gulp 會自動幫你 build js 和 css 到 dist/
+- 再下 gulp watch ，就可以開始實作 js 和 css 了。gulp 會自動幫你 build js 和 css 到 dist/
--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,43 @@
 var gulp = require('gulp');
 
-var coffee = require('gulp-coffee');
 var gutil  = require('gulp-util');
-var compass = require('gulp-compass');
 var cssmin = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 
 var paths = {
-    coffee: ['js/main.coffee', 'js/!(main)*.coffee'],
-    scss: ['css/scss/*.scss']
+    js: ['js/**/*.js'],
+    css: ['css/**/*.css']
 };
 
-gulp.task('compass', function () {
-    gulp.src(paths.scss)
-        .pipe(compass({
-            css: 'dist/css',
-            sass: 'css/scss',
-            image: 'images'
-        }))
+gulp.task('biuld:css', function () {
+    gulp.src(paths.css)
+        .pipe(concat('style.min.css'))
         .pipe(cssmin())
         .pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('coffee', function() {
-    gulp.src(paths.coffee)
-        .pipe(coffee({bare: true}).on('error', gutil.log))
+gulp.task('biuld:js', function() {
+    gulp.src(paths.js)
         .pipe(uglify())
         .pipe(concat('all.min.js'))
         .pipe(gulp.dest('./dist/js/'));
 });
 
-gulp.task('coffee_dev', function() {
-    gulp.src(paths.coffee)
-        .pipe(coffee({bare: true}).on('error', gutil.log))
+gulp.task('biuld:js_dev', function() {
+    gulp.src(paths.js)
         .pipe(concat('all.src.js'))
         .pipe(gulp.dest('./dist/js/'));
 });
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.start('compass', 'coffee', 'coffee_dev');
-    gulp.watch(paths.scss[0], ['compass']);
-    gulp.watch(paths.coffee[0], ['coffee_dev', 'coffee']);
-    gulp.watch(paths.coffee[1], ['coffee_dev', 'coffee']);
+    gulp.start('biuld:css', 'biuld:js', 'biuld:js_dev');
+    gulp.watch(paths.css[0], ['compass']);
+    gulp.watch(paths.js[0], ['biuld:js_dev', 'biuld:js']);
+    gulp.watch(paths.js[1], ['biuld:js_dev', 'biuld:js']);
 });
 
 gulp.task('default', function() {
-    gulp.start('compass', 'coffee', 'coffee_dev');
+    gulp.start('biuld:css', 'biuld:js', 'biuld:js_dev');
 });
